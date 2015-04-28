bd7ffabc2863f70aacffa40b360d8c3409b07a4c
Aravindh Varadharaju <varadharaju.aravindh@gmail.com>
2015-04-28 12:42:14 -0400

    Updated comments in the file for pylint

--- cloudmesh_management/class_generator.py
+++ cloudmesh_management/class_generator.py
@@ -7,6 +7,10 @@ from cmd3.console import Console
 
 
 class CodeGenerator:
+    """
+    The class contains methods that is used to provide the necessary formatting for the python statement
+    to be written to a file.
+    """
     def __init__(self):
         self.level = 0
         self.tab = "    "
@@ -18,26 +22,70 @@ class CodeGenerator:
         return string.join(self.code, "")
 
     def write(self, snippet):
+        """
+        The method is used to add the provided "snippet" according to the tab level. For example,
+            if the level is 0,
+                it appends it as code[some line number] = "snippet"
+            if the level is 1,
+                it appends it as code[some line number] = "    snippet"
+        :param snippet:
+                String to be written to the file
+        :return:
+                None
+        """
         self.code.append(self.tab * self.level + snippet)
 
     def newline(self):
         self.code.append("\n")
 
     def indent(self):
+        """
+        Method is used to add a tab
+        :return:
+        """
         self.level += 1
 
     def dedent(self):
+        """
+        Method is used to remove a tab.
+        :return: None
+        """
         if self.level == 0:
             print "Syntax error in CodeGenerator:", sys.exc_info()
             raise
         self.level -= 1
 
     def write_to_file(self, snippet, filename):
+        """
+        Method is used to write the "snippet" into the "filename" being passed
+        :param snippet:
+                Contains the content of the file to be written
+        :param filename:
+                File name to which the contents would be written to.
+        :return:
+                True is write was successful
+                False, for any issues
+        """
+        target = None
         self.etc_path = os.sep.join(os.getcwd().split(os.sep)[:-1])+"/etc/"
+        if not os.path.exists(self.etc_path):
+            os.makedirs(self.etc_path)
         self.filename = self.etc_path+filename+".py"
-        target = open(self.filename, 'w+')
-        target.write(snippet)
-        target.close()
+        try:
+            target = open(self.filename, 'w+')
+        except IOError:
+            Console.error("Error in opening the file.")
+            return False
+        else:
+            if target:
+                try:
+                    target.write(snippet)
+                except IOError:
+                    Console.error("Error in writing to file")
+                    return False
+            target.close()
+            return True
+
 
 
 class SourceCode:
@@ -47,13 +95,26 @@ class SourceCode:
 
     @classmethod
     def parse(cls, class_type=None):
+        """
+        The method is used to parse the contents of the yaml file and generate the python code for the same. At the
+        moment the method tries to read the file from ~/.cloudmesh. Depending on the class_type, it looks for the file
+        "cloudmesh_user.yaml" or "cloudmesh_project.yaml"
+
+        :param class_type:
+                    class_type can be either user or project.
+
+                    If a collection name is not specified, uses the name of the file as the collection name.
+        :return:
+                Writes the contents to a file under the etc directory. if an "etc" directory does not exist, it creates
+                one. The file name would be either "user.py" or "project.py" depending on the class_type
+        """
         code = CodeGenerator()
         file_path = "~/.cloudmesh/cloudmesh_" + class_type + ".yaml"
         try:
             filename = path_expand(file_path)
             file_config = ConfigDict(filename=filename)
         except:
-            Console.error("Could not load file, please check filename and its path")
+            Console.error("Could not load file, please check filepath: {0}".format(file_path))
             return
         try:
             if class_type == "user":
@@ -122,10 +183,9 @@ class SourceCode:
                         code.newline()
                 code.write("projects = ListField(ReferenceField('Project'))")
                 # print code.end()
-                try:
-                    code.write_to_file(code.end(), class_type)
-                except:
-                    print "Unexpected error:", sys.exc_info()
+
+                if not code.write_to_file(code.end(), class_type):
+                    Console.error("Error while trying to write to file.")
                     pass
 
             elif class_type == "project":
@@ -206,9 +266,12 @@ class SourceCode:
                         code.write(line)
                         code.newline()
                 # print code.end()
-                code.write_to_file(code.end(), class_type)
+                if not code.write_to_file(code.end(), class_type):
+                    Console.error("Error while trying to write to file.")
+                    pass
+
         except:
-            print "Error while reading file"
+            Console.error("Error while reading file.")
         pass
 
 
